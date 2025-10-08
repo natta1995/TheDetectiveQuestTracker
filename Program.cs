1f8baf00b902280f32405b6bb931c6d38409752c
natta1995 <natalie.hallerdal@yahoo.se>
2025-10-08 12:11:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheDetectiveQuestTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome back Detektiv! ");
+        }
+    }
+}
