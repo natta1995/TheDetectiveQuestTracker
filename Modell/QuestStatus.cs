b123400140ebb51991063aed91cc86ecc1752a17
natta1995 <natalie.hallerdal@yahoo.se>
2025-11-02 14:22:13 +0100

    Basic, start. Added.

---
+++ Modell/QuestStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDetectiveQuestTracker.Modell
+{
+    public enum QuestStatus
+    {
+        Available,
+        Accepted
+    }
+
+    public class Quest
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string? OwnerUsername { get; set; }
+        public QuestStatus Status { get; set; } = QuestStatus.Available;
+    }
+}
---
+++ Services/QuestGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheDetectiveQuestTracker.Modell;
+
+namespace TheDetectiveQuestTracker.Services
+{
+    public class MurderQuestGenerator
+    {
+        private readonly Random _rng = new();
+        private readonly string[] victims = { "Greve Lind", "Dr. Åkesson", "Operasångerskan Vega" };
+        private readonly string[] places = { "Gamla Stan", "Observatoriet", "Hamnmagasinet" };
+
+        public Quest Generate()
+        {
+            var v = victims[_rng.Next(victims.Length)];
+            var p = places[_rng.Next(places.Length)];
+            return new Quest
+            {
+                Title = $"Mordet på {v}",
+                Description = $"Offer: {v}\nPlats: {p}\nUppgift: Lös fallet.",
+                Status = QuestStatus.Available
+            };
+        }
+    }
+}
